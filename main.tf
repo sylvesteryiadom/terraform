bb56cdad62dcc906c61199b3f77b09df0dcee2dc
sylvesteryiadom <sylvesteryiadom@gmail.com>
2024-02-17 14:46:38 -0600

    updates to main.tf file

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 provider "aws" {
-  
+  region = "us-east-1"
 }
 
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+}
