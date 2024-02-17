ab726c9d3c2d916ec1335e126ac66685c5f74c87
sylvesteryiadom <sylvesteryiadom@gmail.com>
2024-02-17 15:12:33 -0600

    updating infrastructure

--- main.tf
+++ main.tf
@@ -2,14 +2,30 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = "10.0.0.0/16"
+variable "vpc_cidr_blocks" {}
+variable "subnet_cidr_blocks" {}
+variable "avail_zone" {}
+variable env_prexi{}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_blocks
   tags = {
-    Name = "dev-vpc"
+    Name: "${var.env_prexi}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.dev-vpc.id
-
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_blocks
+  availability_zone = var.avail_zone
+  tags = {
+    Name: "${var.env_prexi}-subnet-1"
+  }
 }
+
+# output "myapp_subnet_outputs" {
+#   value = "test"
+# }
+# output "myapp_vpc_outputs" {
+#   value = "test"
+# }
